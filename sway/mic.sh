b19b73fb8a1a18860fd05ea8007c62e684319c5f
Christian Franco <136227820+Chry55@users.noreply.github.com>
2024-05-26 20:31:38 +0200

    Add files via upload

--- sway/mic.sh
+++ sway/mic.sh
@@ -1,4 +1,6 @@
+lev=$(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Capture) | sed 's/%//g')
 notify-send \
       --hint=string:x-dunst-stack-tag:wallpaper \
       --hint=string:synchronous:wallpaper \
+	  --hint=int:value:$lev \
       " $(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Capture))"
